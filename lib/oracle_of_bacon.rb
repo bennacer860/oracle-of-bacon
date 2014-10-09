d0d67f5427561b6a1dc95030ea7b866bd40b6bab
bennacer860 <bennacer860@gmail.com>
2014-10-09 15:26:46 -0400

    pass the xml parsing specs

--- lib/oracle_of_bacon.rb
+++ lib/oracle_of_bacon.rb
@@ -62,15 +62,37 @@ class OracleOfBacon
     def parse_response
       if ! @doc.xpath('/error').empty?
         parse_error_response
-      # your code here: 'elsif' clauses to handle other responses
-      # for responses not matching the 3 basic types, the Response
-      # object should have type 'unknown' and data 'unknown response'         
+      elsif ! @doc.xpath('/spellcheck').empty?
+        parse_spellcheck_response
+      elsif ! @doc.xpath('/link').empty?
+        parse_graph_response
+      else
+	parse_unknown_response	
       end
     end
+
     def parse_error_response
       @type = :error
       @data = 'Unauthorized access'
     end
+
+    def parse_unknown_response
+      @type = :unknown
+      @data = 'unknown'
+    end
+
+    def parse_spellcheck_response
+      @type = :spellcheck
+      @data = @doc.xpath("//match").map{|e| e.text}	
+    end
+
+
+    def parse_graph_response
+      @type = :graph
+      @data = @doc.xpath("//actor").zip(@doc.xpath("//movie")).flatten.compact.map{|e| e.text}	
+    end
+ 
+
   end
 end
 
